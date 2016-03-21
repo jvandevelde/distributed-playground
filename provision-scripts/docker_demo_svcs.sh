f6fc7218bcb1e2ca709a4e447cae631bde19d67c
julesvandevelde <julesvandevelde@gmail.com>
2016-03-20 19:29:55 -0500

    added restart policies to all Docker containers to make sure they start up on VM restart

--- provision-scripts/docker_demo_svcs.sh
+++ provision-scripts/docker_demo_svcs.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
  
 docker run -d --name demosvc.1.$1.inst1 \
--p 5001:5000 \
--e SERVICE_NAME=demosvc.1 \
--e SERVICE_5000_CHECK_HTTP=/ \
--e SERVICE_5000_CHECK_INTERVAL=5s \
--e SERVICE_TAGS=urlprefix-/ \
--e DOCKER_ENV_VAR1=demosvc.1.$1.inst1 \
+--restart=always                        \
+-p 5001:5000                            \
+-e SERVICE_NAME=demosvc.1               \
+-e SERVICE_5000_CHECK_HTTP=/            \
+-e SERVICE_5000_CHECK_INTERVAL=5s       \
+-e SERVICE_TAGS=urlprefix-/             \
+-e DOCKER_ENV_VAR1=demosvc.1.$1.inst1   \
 jvandevelde/aspnetcoredemo
 
 docker run -d --name demosvc.1.$1.inst2 \
--p 5002:5000 \
--e SERVICE_NAME=demosvc.1 \
--e SERVICE_5000_CHECK_HTTP=/ \
--e SERVICE_5000_CHECK_INTERVAL=5s \
--e SERVICE_TAGS=urlprefix-/ \
--e DOCKER_ENV_VAR1=demosvc.1.$1.inst2 \
+--restart=always                        \
+-p 5002:5000                            \
+-e SERVICE_NAME=demosvc.1               \
+-e SERVICE_5000_CHECK_HTTP=/            \
+-e SERVICE_5000_CHECK_INTERVAL=5s       \
+-e SERVICE_TAGS=urlprefix-/             \
+-e DOCKER_ENV_VAR1=demosvc.1.$1.inst2   \
 jvandevelde/aspnetcoredemo
 
 docker run -d --name demosvc.1.$1.inst3 \
--p 5003:5000 \
--e SERVICE_NAME=demosvc.1 \
--e SERVICE_5000_CHECK_HTTP=/ \
--e SERVICE_5000_CHECK_INTERVAL=5s \
--e SERVICE_TAGS=urlprefix-/ \
--e DOCKER_ENV_VAR1=demosvc.1.$1.inst3 \
+--restart=always                        \
+-p 5003:5000                            \
+-e SERVICE_NAME=demosvc.1               \
+-e SERVICE_5000_CHECK_HTTP=/            \
+-e SERVICE_5000_CHECK_INTERVAL=5s       \
+-e SERVICE_TAGS=urlprefix-/             \
+-e DOCKER_ENV_VAR1=demosvc.1.$1.inst3   \
 jvandevelde/aspnetcoredemo
--- provision-scripts/docker_registry_setup.sh
+++ provision-scripts/docker_registry_setup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
  
-docker run --name consul -h $1      \
--p $1:8300:8300 \
+docker run --name consul -h $1  \
+--restart=always      \
+-p $1:8300:8300       \
 -p $1:8301:8301       \
 -p $1:8301:8301/udp   \
--p $1:8302:8302      \
+-p $1:8302:8302       \
 -p $1:8302:8302/udp   \
 -p $1:8400:8400       \
 -p $1:8500:8500       \
--p 172.17.0.1:53:53/udp      \
+-p 172.17.0.1:53:53/udp         \
 -d -v /mnt:/data progrium/consul -server -advertise $1 -join 172.28.128.10
 
 docker run -h docker-1  \
+--restart=always        \
 -v "/var/run/docker.sock:/tmp/docker.sock"  \
---name registrator  \
+--name registrator      \
 -d gliderlabs/registrator:latest --ip $1 consul://$1:8500
