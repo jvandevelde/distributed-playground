7241f4e95ded459cd8a83d3001c8291d209ad59c
julesvandevelde <julesvandevelde@gmail.com>
2016-04-05 20:49:18 -0500

    -- Added some documentation around debugging/keeping the VirtualBox Guest Addtions up to date with vagrant machines -- Added some sample data sent with logstash to Elasticsearch to demo logging workflow

--- provision-scripts/docker_infrastructure.sh
+++ provision-scripts/docker_infrastructure.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
  
 docker run -d --name logstash --restart=always -v /vagrant/elasticsearch:/config-dir logstash -f /config-dir/logstash.conf
+
+echo 'Writing some sample data for Logstash/Kibana demo'
+cd /tmp
+touch access_log
+echo "Demo message 1" >> access_log
+echo "Demo message 2" >> access_log
+echo "Something's gone horribly wrong!" >> access_log
